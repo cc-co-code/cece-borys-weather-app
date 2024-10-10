a41c56030797e0a98d946692566a155faa6d3962
Borys Kravtsov <mamkinboris3@gmail.com>
2024-10-10 10:42:29 +0200

    local storage small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import List from "./Components/List/List";
 import "./App.css";
 import Form from "./Components/Form/Form";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState(() => {
+    const savedActivities = localStorage.getItem("activities");
+    return savedActivities ? JSON.parse(savedActivities) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(activities));
+  }, [activities]);
   function handleAddActivity(newActivity) {
     setActivities([...activities, newActivity]);
   }
