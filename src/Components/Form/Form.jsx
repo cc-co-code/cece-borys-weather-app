3ffd8e573cdcc2bbb964bfe66e46acb3a1622588
Borys Kravtsov <mamkinboris3@gmail.com>
2024-10-10 17:21:02 +0200

    styling done

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import { uid } from "uid";
-
+import "./Form.css";
 export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,15 +16,37 @@ export default function Form({ onAddActivity }) {
     formElements.name.focus();
   }
   return (
-    <>
-      <h2>Add new Activity</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" required />
-        <label htmlFor="isForGoodWeather">Good Weather Activity:</label>
-        <input type="checkbox" id="isForGoodWeather" name="isForGoodWeather" />
-        <button>submit</button>
+    <section className="form">
+      <h2 className="form__title">Add new Activity</h2>
+      <form className="form__container" onSubmit={handleSubmit}>
+        <label className="form__innerName" htmlFor="name">
+          Name for your activity:
+        </label>
+        <input
+          className="form__name"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="e.g. Swimming"
+          maxLength={"20"}
+          required
+        />
+        <div className="form__checkboxContainer">
+          <label className="form__innerCheckbox" htmlFor="isForGoodWeather">
+            Good weather?
+          </label>
+          <input
+            className="form__checkbox"
+            type="checkbox"
+            id="isForGoodWeather"
+            name="isForGoodWeather"
+          />
+        </div>
+
+        <button className="form__btnSubmit" type="submit">
+          Submit
+        </button>
       </form>
-    </>
+    </section>
   );
 }
